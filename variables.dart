2b6695e26164305e9d8937ac7d0c8777247b0af8
Ira chand <109136391+Chandira77@users.noreply.github.com>
2023-04-25 21:40:32 +0545

    Add files via upload
    this is my variable program

---
+++ variables.dart
@@ -0,0 +1,14 @@
+void main(){
+  //Declaring Variables
+  var name = "Ira";
+  var age = 23;
+  var address = "lokanthali";
+  bool ismarried = false;
+
+  //printing variables value
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print(" Married Status is $ismarried");
+
+}
