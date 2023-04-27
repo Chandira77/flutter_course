d962b7c6c98ca5948541aef300a9aec327033b76
Ira chand <109136391+Chandira77@users.noreply.github.com>
2023-04-27 18:04:23 +0545

    operators
    operators 
    1. Arithemetic operators
    2. Increment operators and decrement operators
    3. Assignment operators
    4.Assignment operators
    5.Logical operators
    6. type test operators

---
+++ operator.dart
@@ -0,0 +1,71 @@
+void main(){
+  /*// declaring two numbers
+  int num1 = 5;
+  double num2 = 3.2;
+  num sum = num1 + num2;
+  num diff = num1 - num2;
+  int unaryminus = -num1;
+  num mul = num1 * num2;
+  num div = num1 / num2;
+  num div2 = num1 ~/ num2;
+  num mod = num1 % num2;
+
+  // printing values
+  print("the addition is $sum.");
+  print("the difference is $diff.");
+  print("the Unary Minus is $unaryminus.");
+  print("the multiply is $mul.");
+  print("the division is $div.");
+  print("the integer division is $div2.");
+  print("the modulus is $mod.");*/
+
+ /* // increment and decrement operators
+  int num1 = 1;
+  int num2 = 0;
+  // pre increment
+  num2 = ++num1;
+  print("The value of num2 is $num2.");
+
+  // reset value to 0
+  num1 = 0;
+  num2 = 0;
+  // post increment
+  num2 = num1++;
+  print("The value of num2 is $num2.");*/
+
+  /*//Assignment operators
+  double age = 22;
+  age += 1;
+  print("after addition age is $age");
+  age -= 1;
+  print("after subtration Age is $age");
+  age *= 1;
+  print("after multiplication Age is $age");
+  age /= 1;
+  print("after division Age is $age");*/
+ 
+ /* //Relational operators
+  int num1 = 10;
+  int num2 = 5;
+  print(num1==num2);
+  print(num1<num2);
+  print(num1>num2);
+  print(num1<=num2);
+  print(num1>=num2); */
+
+ /* //logical operators
+  int userid = 123;
+  int userpin = 456;
+  print((userid == 123) && (userpin == 456));
+  print((userid == 1213) && (userpin == 456));
+  print((userid == 123) || (userpin == 456));
+  print((userid == 1213) || (userpin == 456));
+  print((userid == 123) != (userpin == 456));*/
+
+  //test operator
+  String value1 = "Ira chand";
+  int age = 22;
+  print (value1 is String);
+  print(age is !int);
+
+}
