c5a8ea9e19d4d9ae4e9c450efb8b80e5f9456e5a
Ira chand <109136391+Chandira77@users.noreply.github.com>
2023-04-27 18:01:00 +0545

    comments in dart
    Three types of comments in dart
    1.single-line comment
    2.Double-line comment
    3. multi-line comment

---
+++ comment.dart
@@ -0,0 +1,8 @@
+void main(){
+  // single line comment start with // in dart
+  print("i'm praticing flutter.");
+  /* multi-line comments  */
+  print("This is multi-line comment");
+  //// documenttation comment
+  print("finally this is documentation comment.");
+}
