c308ca37e97f8f9e761f2e53e28cedb2df9b9eed
Ira chand <109136391+Chandira77@users.noreply.github.com>
2023-04-29 21:24:45 +0545

    Userinput in Dart
    Remember this
     {import 'dart:io';}

---
+++ UserInput.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main(){
+  /*string userinput 
+  print("enter name");
+  String? name= 
+  stdin.readLineSync();
+  print("the entered name is ${name}"); */
+
+
+  /* integer userinput 
+   print("enter number");
+   int? number =
+   int. parse(stdin.readLineSync()!);
+   print("The entered number is ${number}"); */
+
+    // Floating userinput 
+   print("enter Decimal number");
+   double? number =
+   double. parse(stdin.readLineSync()!);
+   print("The entered number is ${number}"); 
+
+
+}
